b4ea089809b3627c9ab82d9f20c82b3801e85d8b
Thangamurugan A <128249353+thangamuruga@users.noreply.github.com>
2023-10-07 21:58:33 +0530

    Update test3.py

--- test3.py
+++ test3.py
@@ -2,63 +2,56 @@ import requests
 import pandas as pd
 import os
 
-Params = {"city" : 'london'}
+Params = {
+    "lat": "13.08784",
+    "lon": "80.27847",
+    "appid": "d5dae11bc2489ac50819f2d9ec8d9596",
+    "units": "metric",
+}
 
-Params["city"] = input("Enter the city name")
-
-api_url = 'https://api.api-ninjas.com/v1/geocoding'
-
-response = requests.get(api_url,params= Params, headers={'X-Api-Key': 'F9tx2seTwD/pDYXlAlrMqQ==fNQVAusbN1ooGivJ'})
-
-#print(response.text)
-
-coordinates = response.json()
-
-#print(type(coordinates))
-
-for item in coordinates:
-    if "latitude" in item:
-        Params["lat"] = item["latitude"]
-        print('Your City latitude is :', Params["lat"])
-    if "longitude" in item:
-        Params["lon"] = item["longitude"]
-        print('Your City longitude is :',Params["lon"])
-    if "name" in item:
-        print("Your City Name : ", item["name"])
-
-Params = {"lat": "13.08784", "lon": "80.27847", "appid": "d5dae11bc2489ac50819f2d9ec8d9596", "units": "metric"}
-
+Params["lat"] = float(input("Enter the latitude\t"))
+Params["lon"] = float(input("Enter the latitude\t"))
 
 api_weather = "https://api.openweathermap.org/data/2.5/weather"
 
 response = requests.get(api_weather, params=Params)
 
 values = response.json()
-#print(values)
-#print(type(values))
+# print(values)
+# print(type(values))
 
-for mains,temps in values["main"].items():
-    print(mains , temps)
-print('Kindly follow the Below Points :')
-if values["main"]["temp"] < 30:
-    print("Normal Temperature in your city")
-else :
-    print("We are suggest you to carry water")
+for mains, temps in values["main"].items():
+    # print(mains, temps)
+    if mains == "temp":
+        if temps < 30:
+            print("Normal Temperature in your city")
+        else:
+            print("We are suggest you to carry water")
+    elif mains == "humidity":
+        if temps > 40:
+            print("We are suggest you to carry Umbrella")
+        else:
+            print("Normal humidity in your city")
+    elif mains == 'pressure':
+        if temps > 1000:
+            print("check the pressure value")
+        else:
+            print("normal pressure")
+    else:
+        pass
+        
 
-if values["main"]["humidity"] > 50:
-    print("We are suggest you to carry Umbrella")
-else :
-    print("Normal humidity in your city")
+
+# 'main': {'temp': 31, 'feels_like': 34.36, 'temp_min': 31, 'temp_max': 31, 'pressure': 1008, 'humidity': 58}
 
 
-df =pd.DataFrame(response)
 
 
-df.to_excel('outputstorage3.xlsx',index= False)
+df = pd.DataFrame(response)
+
+
+df.to_excel("outputstorage3.xlsx", index=False)
 
 file_path = os.getcwd()
-
-
-print("check your saved file at",file_path)
 
 
